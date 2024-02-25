886755880ee437d2c0ba2d303e8d24705999482b
Artyom Lysenko <tema199532384@gmail.com>
2024-02-25 21:52:16 +0300

    Added support of non-loop slider (In-Progress)

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const HEIGHT_UNIT = 'rem'
 const UNIT = '%'
-const initialImgWidth = 80 // Frame width in UNIT
-const initialImgHeight = 25 // Frame height in HEIGHT_UNIT
-const amountOfPicturesInSlide = 4 // Number of visible pictures in frame
-const amountOfSlidesPerSlide = 2 // Amount of scrolled pictures per one slide. (amountOfPicturesInSlide + amountOfSlidesPerSlide*2) should not be greater than the whole number of images
+const ENDLESS_SLIDER  = false
+const initialImgWidth = 20 // Frame width in UNIT
+const initialImgHeight = 10 // Frame height in HEIGHT_UNIT
+const amountOfPicturesInSlide = 2 // Number of visible pictures in frame
+const amountOfSlidesPerSlide = 1 // Amount of scrolled pictures per one slide. (amountOfPicturesInSlide + amountOfSlidesPerSlide*2) must not be greater than the whole number of images
 const speed = 400 // Scroll speed in ms
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 const sliderTrack = document.querySelector('.slider-track')
 const wrapper = document.querySelector('.wrapper')
 const transitionTimingFunction = 'cubic-bezier(0, 0, 0.58, 1.0)'
+
 const numberOfInitialDrownSlides = amountOfPicturesInSlide + (amountOfSlidesPerSlide * 2)
-let imgWidth
-if (UNIT != '%') {
-    imgWidth = initialImgWidth / amountOfPicturesInSlide
-} else {
-    imgWidth = 100 / amountOfPicturesInSlide
-}
+
+const imgWidth = UNIT != '%' ? initialImgWidth / amountOfPicturesInSlide : 100 / amountOfPicturesInSlide
+
 const images = [
     './numbers/1.png',
     './numbers/2.png',
@@ -24,11 +23,11 @@ const images = [
     './numbers/4.png', 
     './numbers/5.png', 
     './numbers/6.png', 
-    './numbers/7.png', 
+    './numbers/7.png',
     './numbers/8.png',
-    './numbers/9.png',
-    './numbers/10.png'
+    './numbers/9.png'
 ]
+
 const initialCoords = {
     x : 0
 }
@@ -44,6 +43,8 @@ let indexNext = images.length - amountOfSlidesPerSlide
 let indexPrev = amountOfPicturesInSlide + amountOfSlidesPerSlide
 let slideSwitcher = true
 
+let currentMarginLeftOffset = 0
+
 wrapper.style.width = initialImgWidth + UNIT
 
 const displayedImages = []
@@ -51,56 +52,96 @@ const displayedImages = []
 function drawInitialImages() {
     let index = indexNext
     for (let i = 0; i < numberOfInitialDrownSlides; i++) {
-        if (index === images.length) index = 0
-        const img = drawNewImg(index)
-        if (i === 0) img.style.marginLeft = -(imgWidth * amountOfSlidesPerSlide) + UNIT
-        displayedImages.push(img)
-        sliderTrack.append(img)
-        index++
+        if (ENDLESS_SLIDER) {
+            if (index === images.length) {
+                index = 0
+            } 
+            const img = drawNewImg(index)
+            if (i === 0) {
+                img.style.marginLeft = -(imgWidth * amountOfSlidesPerSlide) + UNIT
+            } 
+            displayedImages.push(img)
+            sliderTrack.append(img)
+            index++
+        } else {
+            const img = drawNewImg(i)
+            displayedImages.push(img)
+            sliderTrack.append(img)
+        }
     }
 }
 
 function slideNext() {
     if (slideSwitcher) {
-        changeFirstImageStyle('0' + UNIT)
-        setTimeout(() => {
-            for (let i = 0; i < amountOfSlidesPerSlide; i++) {
-                indexNext -= 1
-                indexPrev -= 1
-                if (indexNext < 0) indexNext = images.length - 1
-                if (indexPrev < 0) indexPrev = images.length - 1
-                displayedImages[displayedImages.length-1].remove()
-                const img = drawNewImg(indexNext)
-                displayedImages.unshift(img)
-                displayedImages.pop()
-                sliderTrack.prepend(img)
+        if (ENDLESS_SLIDER) {
+            changeFirstImageStyle('0' + UNIT)
+            setTimeout(() => {
+                for (let i = 0; i < amountOfSlidesPerSlide; i++) {
+                    indexNext -= 1
+                    indexPrev -= 1
+                    if (indexNext < 0) indexNext = images.length - 1
+                    if (indexPrev < 0) indexPrev = images.length - 1
+                    displayedImages[displayedImages.length-1].remove()
+                    const img = drawNewImg(indexNext)
+                    displayedImages.unshift(img)
+                    displayedImages.pop()
+                    sliderTrack.prepend(img)
+                }
+                finishAnimation()
+            }, speed)
+        } else {
+            if (currentMarginLeftOffset < 0) {
+                changeFirstImageStyle((currentMarginLeftOffset + (imgWidth * amountOfSlidesPerSlide)) + UNIT)
+                currentMarginLeftOffset += imgWidth * amountOfSlidesPerSlide
+                setTimeout(() => {
+                    slideSwitcher = true
+                }, speed)
             }
-            finishAnimation()
-        }, speed)
-    } else {
-        return
+        }
     }
 }
 
 function slidePrev() {
     if (slideSwitcher) {
-        changeFirstImageStyle(-imgWidth * (2 * amountOfSlidesPerSlide) + UNIT)
-        setTimeout(() => {
-            for (let i = 0; i < amountOfSlidesPerSlide; i++) {
-                indexPrev += 1
-                indexNext += 1
-                if (indexPrev > images.length - 1) indexPrev = 0
-                if (indexNext > images.length - 1) indexNext = 0
-                displayedImages[0].remove() 
-                const img = drawNewImg(indexPrev)
-                displayedImages.push(img)
-                displayedImages.shift()
-                sliderTrack.append(img)
+        if (ENDLESS_SLIDER) {
+            changeFirstImageStyle(-imgWidth * (2 * amountOfSlidesPerSlide) + UNIT)
+            setTimeout(() => {
+                for (let i = 0; i < amountOfSlidesPerSlide; i++) {
+                    if (indexPrev > images.length - 1) indexPrev = 0
+                    if (indexNext > images.length - 1) indexNext = 0
+                    displayedImages[0].remove() 
+                    const img = drawNewImg(indexPrev)
+                    displayedImages.push(img)
+                    displayedImages.shift()
+                    sliderTrack.append(img)
+                    indexPrev += 1
+                    indexNext += 1
+                }
+                finishAnimation()
+            }, speed)
+        } else {
+            if (currentMarginLeftOffset <= 0) {
+
+                changeFirstImageStyle((currentMarginLeftOffset - (imgWidth * amountOfSlidesPerSlide)) + UNIT)
+                
+                currentMarginLeftOffset -= imgWidth * amountOfSlidesPerSlide
+                
+                setTimeout(() => {
+                    const newImages = []
+                    for (let i = displayedImages.length; i < displayedImages.length + amountOfSlidesPerSlide; i++) {
+                        if (images[i]) {
+                            const img = drawNewImg(i)
+                            newImages.push(img)
+                        }
+                    }
+                    newImages.forEach(image => {
+                        displayedImages.push(image)
+                        sliderTrack.append(image)
+                    })
+                    slideSwitcher = true
+                }, speed)
             }
-            finishAnimation()
-        }, speed)
-    } else {
-        return
+        }
     }
 }
 
